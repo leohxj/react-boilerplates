baaebf4a49f6b7add32570d535b50eadf4051426
Leo Hui <leohxj@gmail.com>
2017-12-18 10:46:55 +0800

    ✨  feat: add eslint-config-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,22 @@
 module.exports = {
-  "extends": "airbnb-base",
-  "env": {
-    "browser": true,
-    "node": true,
-    "es6": true,
-    "commonjs": true,
+  extends: ['airbnb-base', 'prettier'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    commonjs: true
   },
   parserOptions: {
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  "plugins": [
-    "import"
-  ],
+  plugins: ['import'],
   rules: {
-    "class-methods-use-this": "off",
-    "comma-dangle": "off",
-    "global-require": "off",
-    "import/no-extraneous-dependencies": "off",
-    "import/prefer-default-export": "off",
-    "max-len": "off",
-    "no-underscore-dangle": "off"
+    'class-methods-use-this': 'off',
+    'comma-dangle': 'off',
+    'global-require': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off',
+    'max-len': 'off',
+    'no-underscore-dangle': 'off'
   }
 };
