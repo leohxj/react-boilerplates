359ba1dfdc572ba8e3541d93ed122d04b1411fc0
Leo Hui <leohxj@gmail.com>
2018-06-26 17:03:39 +0800

    chore: update react

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,17 @@
-// @flow
+/**
+ * @file 入口文件
+ * @description 是否有必要拆分 Layout, 值得商量， 我这么做是为了文件结构清晰
+ *
+ * @author Leo Hui <leohxj@gmail.com>
+ *
+ * Created Date: Tue, 2018-01-09 17:12:51
+ *
+ * Last Modified: Tue, 2018-06-26 16:55:36
+ * Last Modified By: Leo Hui <leohxj@gmail.com>
+ *
+ * @flow
+ */
+
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 
--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,14 +1,15 @@
 /**
- * @file 页面根节点
+ * @file 页面根节点, 文件夹为 layouts， 寓意页面基本结构
  * @description
- * redux 放在应用的入口去注册. Router 相关初始化放在这里
- * 这个管理应用整体的结构, 理论上应用 Header, Footer 都是固定的吧
+ * 完成 Router， ReactHotLoader 的注册
+ * Redux 的注册， 放在也Main 组件中 （为什么没有放在这一层？？）
+ * 这个管理应用整体的结构, 理论上应用 Header, Footer 都是固定的
  *
  * @author Leo Hui <leohxj@gmail.com>
 
  * Created Date: Fri, 2018-01-12 16:43:13
  *
- * Last Modified: Tue, 2018-06-26 16:44:24
+ * Last Modified: Tue, 2018-06-26 16:58:21
  * Last Modified By: Leo Hui <leohxj@gmail.com>
  *
  * @flow
